704f2b6dd68a39d864652a8d2c9f536c9fd0d68e
Luan Almeida <luan.rego01@gmail.com>
2023-06-29 23:56:04 -0300

    Resolution of the second financial year

--- Exercicio 2/exercicio2.js
+++ Exercicio 2/exercicio2.js
@@ -5,3 +5,28 @@
  * a tradução para a mesma cor em português. Utiliza uma estrutura condicional (switch)
  * para fazer o mapeamento das cores.
  */
+
+//função criada para exibir um resultado.
+function showResult(frase) {
+    document.write(frase);
+}
+
+let color = prompt("Escolha uma cor primaria em inglês");
+
+function colorTranslation() {
+    switch (color) {
+        case "red":
+            showResult("Tradução de Red é: Vermelho");
+            break;
+        case "blue":
+            showResult("Tradução de Blue é: Azul");
+            break;
+        case "green":
+            showResult("Tradução de Green é: Verde");
+            break;
+        default:
+            showResult("Cor inválida");
+            break;
+    }
+}
+colorTranslation();
