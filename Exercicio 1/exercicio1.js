e5978db52d77801a59e3572295dde81f605932f2
Luan Almeida <luan.rego01@gmail.com>
2023-06-29 23:33:25 -0300

    Resolution of the first financial year

--- Exercicio 1/exercicio1.js
+++ Exercicio 1/exercicio1.js
@@ -6,3 +6,24 @@
  * adulto (18-59 anos) ou idoso (60 anos ou mais). Utiliza uma estrutura condicional
  * (if, else if, else).
  */
+
+//função criada para exibir um resultado.
+function showResult(frase) {
+    document.write(frase);
+}
+let yourName = prompt("Informe seu nome");
+let age = parseFloat(prompt(yourName + " ,qual a sua idade?"));
+
+function determinesAge(age) {
+    if (age >= 0 && age <= 12) {
+        showResult(yourName + ", você é considerado(a) uma criança!");
+    }else if(age >= 13 && age <= 17){
+        showResult(yourName + ", você é considerado(a) um adolescente!");
+    }else if(age >= 19 && age <= 59){
+        showResult(yourName + ", você é considerado(a) um adulto(a)!");
+    }else if (age >= 60){
+        showResult(yourName + ", você é considerado(a) um idoso(a)!");
+    }
+}
+
+determinesAge(age);
