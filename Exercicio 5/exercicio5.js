c659afcdde78902c34a00758ccb6476e48768e64
Luan Almeida <luan.rego01@gmail.com>
2023-07-03 22:48:32 -0300

    resolution of the fifth exercise

--- Exercicio 5/exercicio5.js
+++ Exercicio 5/exercicio5.js
@@ -4,3 +4,19 @@
  * Escreve um programa que percorre um array de palavras e exibe todas as palavras que possuem
  * mais de 5 caracteres. Utiliza a estrutura de repetição for of para percorrer o array.
  */
+ 
+//função criada para exibir um resultado.
+function showResult(frase) {
+    document.write(frase);
+    document.write("<br>");
+}
+
+let words = [
+    "biscoito","praia","lazer","sol","montanha","céu","paralelepipido"
+];
+
+for (const word of words) {
+    if(word.length > 5){
+    showResult(word);
+    }
+}
