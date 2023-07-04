1a599ae9db0c0e4bc66af1710b131720b77754a0
Luan Almeida <luan.rego01@gmail.com>
2023-07-03 22:36:10 -0300

    Resolution of the fourth exercise

--- Exercicio 4/exercicio4.js
+++ Exercicio 4/exercicio4.js
@@ -6,3 +6,24 @@
  * 0, 1, 1, 2, 3, 5, 8, 13, ...
  * Utiliza uma estrutura de repetição (while) para gerar a sequência.
  */
+
+//função criada para exibir um resultado.
+function showResult(frase) {
+    document.write(frase);
+    document.write("<br>");
+}
+
+let number = parseFloat(prompt("Insira um número de 0 a 1000"));
+let limit = 1000;
+let currentNumber = number;
+let previousNumber = 1;
+
+while (currentNumber < limit){
+    showResult(number)
+    number = previousNumber + currentNumber;
+    previousNumber = currentNumber;
+    currentNumber = number;
+}
+
+
+
