41b9f394590880f9c705f24249b3d61902a26426
Luan Almeida <luan.rego01@gmail.com>
2023-07-06 22:34:58 -0300

    resolution of the eighth exercise

--- Exercicio 8/exercicio8.js
+++ Exercicio 8/exercicio8.js
@@ -5,3 +5,27 @@
  * matemática. A função `calculadora` executa a operação matemática nos dois números fornecidos e retorna o resultado.
  * Testa a função utilizando diferentes operações como adição, subtração, multiplicação e divisão.
  */
+function showResult(frase) {
+    document.write(frase);
+    document.write("<br>");
+}
+
+alert("Função calculadora, vamos calcular dois números");
+let numberOne = parseFloat(prompt("Informe o primeiro número"));
+let numberTwo = parseFloat(prompt("Informe o segundo número"));
+let operator = prompt("Somar '+' | Diminuir '-' | Multiplicar '*' | Dividir '/'");
+
+function calculator(numberOne, numberTwo){
+    switch(operator) {
+        case "+":
+            return numberOne + numberTwo;
+        case "-":
+            return numberOne - numberTwo;
+        case "*":
+            return numberOne * numberTwo;
+        case "/":
+            return numberOne / numberTwo;
+    } 
+}
+
+showResult(calculator(numberOne, numberTwo, operator));
